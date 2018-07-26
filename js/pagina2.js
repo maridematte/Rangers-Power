ad676c7eb8dbc00dee666cacccd6ffe616bd5ad4
maridematte <marianag.dematte@gmail.com>
2018-07-26 12:21:39 -0300

    Mudando para colocar aquele negocio novo

---
+++ js/pagina2.js
@@ -0,0 +1,21 @@
+var movies = []
+
+for (item of movies) {
+  var mov_div = document.createElement("div");
+  mov_div.className = "col-md-4";
+  var poster = document.createElement("img");
+  
+  mov_div.appendChild(poster);
+}
+
+
+function car_elements () {
+  var prime_filme = movies[0]
+  var first_ele = document.createElement("div")
+  first_ele.className = "carousel-item active"
+  var row_car =
+  }
+
+}
+
+document.getElementById("carousel_inn").innerHTML =
